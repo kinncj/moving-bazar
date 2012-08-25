df1092071ff8f87dbc84a3c1e59c603c757b1b23
Kinn Coelho Julião <kinncj@gmail.com>
2012-08-25 17:17:22 -0300

    Mail settings

--- src/MovingBazar/Controller/About.php
+++ src/MovingBazar/Controller/About.php
@@ -5,6 +5,6 @@ class About extends AbstractController
 {
     public function get()
     {
-    	$this->render('about/index.html', array('about'=> file_get_contents(CONFIG_PATH.'about.ini')));
+    	$this->render('about/index.html', array('about'=> nl2br(file_get_contents(CONFIG_PATH.'about.ini'))));
     }
 }
