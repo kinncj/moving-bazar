9d7bae1e9c85e11cef438c2a9b350f08b5d512b3
Kinn Coelho Julião <kinncj@gmail.com>
2012-08-25 15:37:12 -0300

    Product routing

--- src/MovingBazar/Controller/Product.php
+++ src/MovingBazar/Controller/Product.php
@@ -7,6 +7,6 @@ class Product extends AbstractController
     public function get($name)
     {
     	$products = new ProductRepository;
-    	$this->render('product/index.html', array('product'=> array_chunk($products->findByName($name), 4, true)));
+    	$this->render('product/index.html', array('product'=> $products->findByName($name)));
     }
 }
